b28fd2a383aaa94a8f412a44d0d7858e8ca41a84
WiseBoiii <aleksashin.1001@gmail.com>
2022-10-15 17:16:53 +0300

    Added an ASCII table for HH vacancies
    In progress: ASCII table for SJ vacancies

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from pprint import pprint
 from dotenv import load_dotenv
 import os
+from terminaltables import AsciiTable
 
 
 def get_hh_vacancies(language):
@@ -41,33 +42,75 @@ def predict_rub_salary(salary_from, salary_to, salary_currency):
     elif salary_to:
         return int(salary_to) * 0.8
 
+def make_table(languaged_vacancies, company):
+    table_payload = [
+        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
+    ]
+    for lang, lang_params in languaged_vacancies.items():
+        table_payload.append(
+            [
+                lang,
+                lang_params['vacancies_found'],
+                lang_params['vacancies_processed'],
+                lang_params['average_salary']
+            ])
+    if company == 'sj':
+        actual_table = AsciiTable(table_payload, title='SuperJob Moscow')
+    if company == 'hh':
+        actual_table = AsciiTable(table_payload, title='HeadHunter Moscow')
+    return actual_table
+
 
 if __name__ == '__main__':
     load_dotenv()
     languages = ['TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'C#', 'C++', 'PHP', 'Ruby',
                  'Python', 'Java', 'JavaScript']
     sj_token = os.environ['SJ_TOKEN']
-    salaries = []
-    languaged_vacancies = {}
+    hh_salaries, sj_salaries = [], []
+    hh_languaged_vacancies, sj_languaged_vacancies = {}, {}
     for language in languages:
         try:
             hh_vacancies_found, hh_vacancies = get_hh_vacancies(language)
             sj_vacancies_found, sj_vacancies = get_sj_vacancies(language, sj_token)
         except requests.exceptions.HTTPError:
             continue
-        predicted_sj_salary = predict_rub_salary(sj_vacancies['payment_from'], sj_vacancies['payment_to'],
-                                                 sj_vacancies['currency'])
-        #Написать цикл для формирования зп и словаря
+        for vacancy in sj_vacancies:
+            if not vacancy.get('salary'):
+                continue
+            predicted_sj_salary = predict_rub_salary(
+                sj_vacancies['payment_from'],
+                sj_vacancies['payment_to'],
+                sj_vacancies['currency']
+            )
+            if predicted_sj_salary:
+                hh_salaries.append(predicted_sj_salary)
+        average_salary = 0
+        if sj_salaries:
+            sj_average_salary = int(sum(sj_salaries) / len(sj_salaries))
+            sj_languaged_vacancies[language] = {
+                'vacancies_found': sj_vacancies_found,
+                'vacancies_processed': len(sj_salaries),
+                'average_salary': sj_average_salary
+            }
         for vacancy in hh_vacancies:
             if not vacancy.get('salary'):
                 continue
-            predicted_hh_salary = predict_rub_salary(vacancy['salary']['from'], vacancy['salary']['to'],
-                                                     vacancy['salary']['currency'])
+            predicted_hh_salary = predict_rub_salary(
+                vacancy['salary']['from'],
+                vacancy['salary']['to'],
+                vacancy['salary']['currency']
+                )
             if predicted_hh_salary:
-                salaries.append(predicted_hh_salary)
+                hh_salaries.append(predicted_hh_salary)
         average_salary = 0
-        if salaries:
-            average_salary = int(sum(salaries) / len(salaries))
-        languaged_vacancies[language] = {'vacancies_found': hh_vacancies_found, 'vacancies_processed': len(salaries),
-                                         'average_salary': average_salary}
-    pprint(languaged_vacancies)
+        if hh_salaries:
+            hh_average_salary = int(sum(hh_salaries) / len(hh_salaries))
+            hh_languaged_vacancies[language] = {
+                'vacancies_found': hh_vacancies_found,
+                'vacancies_processed': len(hh_salaries),
+                'average_salary': hh_average_salary
+            }
+    #sj_table = make_table(sj_languaged_vacancies, 'sj')
+    hh_table = make_table(hh_languaged_vacancies, 'hh')
+    #print(sj_table.table)
+    print(hh_table.table)
