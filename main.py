4704134bfd284b0d1024c1f3573bd6490c1e75a5
WiseBoiii <64275846+WiseBoiii@users.noreply.github.com>
2023-10-28 16:51:38 +0300

    Minor fix
    Removed unneccesary condition

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from headhunter import get_hh_statistics
 from superjob import get_sj_statistics
 
 
-def make_table(languaged_vacancies, company):
+def make_table(languaged_vacancies, company_name):
     table_payload = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
     ]
@@ -15,16 +15,15 @@ def make_table(languaged_vacancies, company):
                 lang_params['vacancies_processed'],
                 lang_params['average_salary']
             ])
-    if company == 'sj':
-        actual_table = AsciiTable(table_payload, title='SuperJob Moscow')
-    if company == 'hh':
-        actual_table = AsciiTable(table_payload, title='HeadHunter Moscow')
+
+        actual_table = AsciiTable(table_payload, company_name)
+
     return actual_table
 
 
 def main():
-    hh_vacancies = make_table(get_hh_statistics(), 'hh')
-    sj_vacancies = make_table(get_sj_statistics(), 'sj')
+    hh_vacancies = make_table(get_hh_statistics(), 'HeadHunter Moscow')
+    sj_vacancies = make_table(get_sj_statistics(), 'SuperJob Moscow')
     print(hh_vacancies.table, '\n', sj_vacancies.table)
 
 
